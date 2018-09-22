0e8d1d42fa3158a000a4200b1b2ae24122a9d4ea
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-21 21:42:18 -0400

    Add some more fields to Shortcut class

--- index.js
+++ index.js
@@ -5,11 +5,52 @@ const baseURL = "https://www.icloud.com/shortcuts/api/records/";
 const baseLink = "https://www.icloud.com/shortcuts/";
 
 class Shortcut {
-	constructor(data) {
+	constructor(data, id) {
+		/**
+		 * The user-specified name of the shortcut.
+		 */
 		this.name = data.fields.name.value;
-		this.id = response.body.id;
+
+		/**
+		 * The long description of the shortcut.
+		 * This does not seem to be settable by users.
+		 */
+		this.longDescription = data.fields.longDescription.value;
+
+		/**
+		 * The date that the shortcut was created.
+		 */
+		this.creationDate = new Date(data.created.timestamp);
+
+		/**
+		 * The date that the shortcut was last modified on.
+		 */
+		this.modificationDate = new Date(data.modified.timestamp);
+
+		/**
+		 * The URL to download the shortcut's icon.
+		 */
+		this.imageURL = data.fields.icon.value.downloadURL;
+
+		/**
+		 * The URL to download the shortcut as a PLIST.
+		 */
+		this.downloadURL = data.fields.shortcut.value.downloadURL;
+
+		/**
+		 * The full API response.
+		 */
+		this.response = data;
+
+		/**
+		 * The ID of the shortcut.
+		 */
+		this.id = id;
 	}
 	
+	/**
+	 * Returns a link to the landing page for the shortcut.
+	 */
 	getLink() {
 		return baseLink + this.id;
 	}
@@ -31,7 +72,7 @@ function getShortcutDetails(id) {
 		got(baseURL + id, {
 			json: true,
 		}).then(response => {
-			resolve(new Shortcut(response.body));
+			resolve(new Shortcut(response.body, id));
 		}).catch(reject);
 	});
 }
