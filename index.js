11fd646d376ed8042b8f28b06c8f2fb952c9254b
Leo MG Nesfield (LMGN) <contact@thelmgn.com>
2018-10-07 00:03:02 +0100

    Usability enhancement: Include pre-converted hexadecimal color

--- index.js
+++ index.js
@@ -177,11 +177,13 @@ class Shortcut {
 		/**
 		 * Details of the icon of this shortcut.
 		 * @property {number} color The color of the shortcut's icon, with transparency.
+		 * @property {string} hexColor The hexadecimal color of the shortcut's icon, with transparency.
 		 * @property {string} downloadURL The URL to download the shortcut's icon.
 		 * @property {number} glyph The ID of the glyph.
 		 */
 		this.icon = {
 			color: data.fields.icon_color.value,
+			hexColor: data.fields.icon_color.value.toString(16),
 			downloadURL: data.fields.icon.value.downloadURL,
 			glyph: data.fields.icon_glyph.value,
 		};
