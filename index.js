328d83ce293c32e56691281c6019261881bb31f7
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-10-07 09:36:42 -0400

    Fix naming of hexadecimal icon color property

--- index.js
+++ index.js
@@ -177,15 +177,15 @@ class Shortcut {
 		/**
 		 * Details of the icon of this shortcut.
 		 * @property {number} color The color of the shortcut's icon, with transparency.
-		 * @property {string} hexColor The hexadecimal color of the shortcut's icon, with transparency.
 		 * @property {string} downloadURL The URL to download the shortcut's icon.
 		 * @property {number} glyph The ID of the glyph.
+		 * @property {string} hexColor The hexadecimal color of the shortcut's icon.
 		 */
 		this.icon = {
 			color: data.fields.icon_color.value,
-			colorHex: data.fields.icon_color.value.toString(16).substring(0, 6),
 			downloadURL: data.fields.icon.value.downloadURL,
 			glyph: data.fields.icon_glyph.value,
+			hexColor: data.fields.icon_color.value.toString(16).substring(0, 6),
 		};
 
 		/**
