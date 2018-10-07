549dbd773ee59ceea8fd3d52ccb830c46b91c7ad
Leo MG Nesfield (LMGN) <leonesfield+github@gmail.com>
2018-10-07 13:11:59 +0100

    Guess how often I use Eslint lol

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ class Shortcut {
 		 */
 		this.icon = {
 			color: data.fields.icon_color.value,
-			colorHex: data.fields.icon_color.value.toString(16).substring(0,6), 
+			colorHex: data.fields.icon_color.value.toString(16).substring(0, 6),
 			downloadURL: data.fields.icon.value.downloadURL,
 			glyph: data.fields.icon_glyph.value,
 		};
